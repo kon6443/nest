a52e25ee56c546e31a84c4ae07078026a3437720
Onam Kwon <kon6443@gmail.com>
2023-06-14 14:20:43 +0900

    Forum is renamed to articles.

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
 
-@Controller('forum')
-export class ForumController {
+@Controller('articles')
+export class ArticlesController {
     @Get()
     handleGetMain(): string {
         console.log('This is a main forum.');
         return 'This is a main page of the forum.';
     }
 
-    @Get('req')
+    @Get('2')
     handleGetPracticeReqObject(@Req() req: Request): string {
         console.log('req:', req);
         return 'practice request object.';
--- src/forum/forum.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ForumController } from './forum.controller';
-import { ForumService } from './forum.service';
-
-@Module({
-  controllers: [ForumController],
-  providers: [ForumService]
-})
-export class ForumModule {}
