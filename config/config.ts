27d2cef180a07463bcd90d22b46133130da60242
Onam Kwon <kon6443@gmail.com>
2023-06-15 23:36:03 +0900

    Connected config.ts and .env

--- config/config.ts
+++ config/config.ts
@@ -1,22 +1,23 @@
 import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const envFilePath = path.resolve(__dirname, '../../config/.env');
 
 // Load the .env file
-dotenv.config();
+dotenv.config({path: envFilePath});
 
 /**
  * Reason why to use both config.js and .env file is to use auto complete feature.
  * And config is manipulated by registering into a container.
  */
 export const config = {
-
     // MySQL DB connection
     MYSQL: {
-        // HOST: process.env.SQL_HOST,
-        USER: process.env.SQL_USER,
-        PASSWORD: process.env.SQL_PASSWORD,
+        HOST: process.env.MYSQL_HOST,
+        USER: process.env.MYSQL_USER,
+        PASSWORD: process.env.MYSQL_PASSWORD,
         DATABASE: process.env.MYSQL_DATABASE,
         ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD
     }
-
 };
 
