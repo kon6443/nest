18baca201f370421ebbcfec944990944dcaa61d9
Onam Kwon <kon6443@gmail.com>
2023-06-17 00:45:04 +0900

    Added ejs template engine setting.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import * as express from 'express'; 
 import * as path from 'path'; 
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     
     // Set the static file serving.
-    app.use(express.static(path.join(__dirname, '..', 'public')));
+    app.use('/public', express.static(path.join(__dirname, '../..', 'public')));
+
+    app.setViewEngine('ejs');
+    app.setBaseViewsDir(path.join(__dirname, '../..', 'views')); // Set the directory where your views/templates are located
 
     await app.listen(3000);
 }
