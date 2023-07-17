1af459a2d8efb867d332983c290f4dc4b6312c42
Onam Kwon <kon6443@gmail.com>
2023-07-17 12:54:34 +0900

    Added chat module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ArticlesModule } from './articles/articles.module';
 import { UserModule } from './user/user.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ArticlesModule, UserModule, SharedModule, AuthModule],
+  imports: [ArticlesModule, UserModule, SharedModule, AuthModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
