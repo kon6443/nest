b94b437f388df692316f01e1441e724bd4ab373e
Onam Kwon <kon6443@gmail.com>
2023-07-19 23:23:29 +0900

    Added WebSockets.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as socketio from 'socket.io';
 
 import * as express from 'express'; 
 import * as path from 'path'; 
@@ -12,7 +14,9 @@ async function bootstrap() {
     
     // Set the static file serving.
     app.use('/public', express.static(path.join(__dirname, '../..', 'public')));
-
+    
+    app.useWebSocketAdapter(new IoAdapter(app.getHttpServer()));
+    
     app.setViewEngine('ejs');
     app.setBaseViewsDir(path.join(__dirname, '../..', 'views')); // Set the directory where your views/templates are located
 
