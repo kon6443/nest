f416bb4755465fb9f618886b2920238422063049
Onam Kwon <kon6443@gmail.com>
2023-07-19 23:23:05 +0900

    Working on chat.

--- public/javascript/socket_io.js
+++ public/javascript/socket_io.js
@@ -13,14 +13,42 @@
 //     });
 // }
 
+let socket = io.connect('/chat', {
+    path: '/socket.io',
+    transports: ['websocket'],
+});
+
+// receiving a message
+socket.on('chat', function (data) {
+    // Get the chat-content element
+    const chatContentElement = document.getElementById('chat-content');
+
+    let text = document.createElement('div');
+    text.textContent = data.announcement ? data.announcement : `${data.id}: ${data.message}`;
+
+    if(data.id===socket.id) {
+        text.classList.add('chat-message', 'sent');
+    } else {
+        text.classList.add('chat-message', 'received');
+    }
+
+    // Append the row to the chat-content element
+    chatContentElement.appendChild(text);
+    chatContentElement.scrollTop = chatContentElement.scrollHeight;
+});
+
+
 function sendMsg() {
-	const title = document.getElementById('title').value;
+	const message = document.getElementById('chat-type').value;
+    document.getElementById('chat-type').value = '';
+    // socket.emit() method sends a message to the server.
+    socket.emit('chat', message);
 }
 
-
-$("#chat-type").on("keyup", function () {
-    if(window.event.keyCode==13 && $(this).val()!=""){
+document.getElementById('chat-type').addEventListener('keypress', function(e) {
+    const value = document.getElementById('chat-type').value;
+    if(e.keyCode==13 && value!='') {
         sendMsg();
     }
-});
+})
 
--- src/websockets/websockets.gateway.ts
+++ src/websockets/websockets.gateway.ts
@@ -1,7 +1,7 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: '/chat' })
 export class WebsocketsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server: Server;
@@ -9,11 +9,25 @@ export class WebsocketsGateway implements OnGatewayConnection, OnGatewayDisconne
     // WebSocket event handlers and business logic
     
     handleConnection(client: Socket) {
-        console.log(`${client.id} has entered the chat room.`);
+        const announcement = `${client.id} has entered the chat room.`;
+        console.log(announcement);
+        // console.log(client.handshake.headers.cookie.substr(4));
+        this.server.emit('chat', { announcement: announcement} );
     }
 
     handleDisconnect(client: Socket) {
-        console.log(`${client.id} has left the chatroom.`);
+        const announcement = `${client.id} has left the chatroom.`;
+        console.log(announcement);
+        this.server.emit('chat', { announcement: announcement} );
+    }
+
+    @SubscribeMessage('chat')
+    handleMessage(client: Socket, message: string) {
+        // console.log('client:', client.id);
+        // console.log('message:', message);
+        console.log(`${client.id}: ${message}`);
+        // this.server.emit('chat', message);
+        this.server.emit('chat', { id: client.id, message: message });
     }
 
 }
